64984b77e717c3d9426326b9c49b5a5258d4602d
Patrice Chotard <patrice.chotard@foss.st.com>
2023-11-22 17:53:28 +0100

    mtd: cfi_flash: Retrieve memory-map area using "memory-map" property
    For some platforms, reg property can be used to indicate the chip select
    which is managed at driver level.
    In this case, the memory-map area can be found in the memory-region
    property.
    
    Update cfi framework to accept this additional bindings.
    
    Signed-off-by: Patrice Chotard <patrice.chotard@foss.st.com>
    Change-Id: Id37b666d23992042061098c14198cd42b8a86756
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/349179

--- drivers/mtd/cfi_flash.c
+++ drivers/mtd/cfi_flash.c
@@ -31,10 +31,12 @@
 #include <asm/io.h>
 #include <asm/byteorder.h>
 #include <asm/unaligned.h>
+#include <dm/device_compat.h>
 #include <dm/of_access.h>
 #include <dm/ofnode.h>
 #include <env_internal.h>
 #include <linux/delay.h>
+#include <linux/ioport.h>
 #include <linux/sizes.h>
 #include <mtd/cfi_flash.h>
 #include <mtd/sfdp_flash.h>
@@ -2675,7 +2677,9 @@ unsigned long flash_init(void)
 static int cfi_flash_probe(struct udevice *dev)
 {
 	fdt_addr_t addr;
-	int idx;
+	struct ofnode_phandle_args args;
+	struct resource res;
+	int idx, ret;
 
 	/*
 	 * first, check if parent's node has a "status" property
@@ -2684,15 +2688,31 @@ static int cfi_flash_probe(struct udevice *dev)
 	if (!dev_read_enabled(dev_get_parent(dev)))
 		return -ENODEV;
 
-	for (idx = 0; idx < CFI_MAX_FLASH_BANKS; idx++) {
-		addr = dev_read_addr_index(dev, idx);
-		if (addr == FDT_ADDR_T_NONE)
-			break;
+	ret = dev_read_phandle_with_args(dev_get_parent(dev), "memory-region", NULL, 0, 0, &args);
+	if (!ret) {
+		for (idx = 0; idx < CFI_MAX_FLASH_BANKS; idx++) {
+			ret = ofnode_read_resource(args.node, idx, &res);
+			if (ret) {
+				dev_err(dev, "Can't get mmap base address(%d)\n", ret);
+				return ret;
+			}
 
-		flash_info[cfi_flash_num_flash_banks].dev = dev;
-		flash_info[cfi_flash_num_flash_banks].base = addr;
-		cfi_flash_num_flash_banks++;
+			flash_info[cfi_flash_num_flash_banks].dev = dev;
+			flash_info[cfi_flash_num_flash_banks].base = res.start;
+			cfi_flash_num_flash_banks++;
+		}
+	} else {
+		for (idx = 0; idx < CFI_MAX_FLASH_BANKS; idx++) {
+			addr = dev_read_addr_index(dev, idx);
+			if (addr == FDT_ADDR_T_NONE)
+				break;
+
+			flash_info[cfi_flash_num_flash_banks].dev = dev;
+			flash_info[cfi_flash_num_flash_banks].base = addr;
+			cfi_flash_num_flash_banks++;
+		}
 	}
+
 	gd->bd->bi_flashstart = flash_info[0].base;
 
 	return 0;
