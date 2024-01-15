100ab7a602f6531a699639cef5a945ec1c97e616
Yannick Fertre <yannick.fertre@foss.st.com>
2024-01-15 11:44:18 +0100

    video: stm32: dsi: add power supply support
    Add support of regualtor vdda18.
    
    Change-Id: I6fe6bbc049f9b81c32ac552547505c10517281c8
    Signed-off-by: Yannick Fertre <yannick.fertre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/351298
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Patrice CHOTARD <patrice.chotard@foss.st.com>

--- drivers/video/stm32/stm32_dsi.c
+++ drivers/video/stm32/stm32_dsi.c
@@ -143,6 +143,7 @@ struct stm32_dsi_priv {
 	int lane_min_kbps;
 	int lane_max_kbps;
 	struct udevice *vdd_reg;
+	struct udevice *vdda18_reg;
 	struct udevice *dsi_host;
 	unsigned int lane_mbps;
 	u32 format;
@@ -986,6 +987,19 @@ static int stm32_dsi_probe(struct udevice *dev)
 			return ret;
 	}
 
+	ret =  device_get_supply_regulator(dev, "vdda18",
+					   &priv->vdda18_reg);
+	if (ret && ret != -ENOENT) {
+		dev_err(dev, "Warning: cannot get vdda18 supply\n");
+		return ret;
+	}
+
+	if (ret != -ENOENT) {
+		ret = regulator_set_enable(priv->vdda18_reg, true);
+		if (ret)
+			return ret;
+	}
+
 	ret = clk_get_by_name(device->dev, "pclk", &clk);
 	if (ret) {
 		dev_err(dev, "peripheral clock get error %d\n", ret);
@@ -1032,6 +1046,7 @@ static int stm32_dsi_probe(struct udevice *dev)
 err_clk:
 	clk_disable(&clk);
 err_reg:
+	regulator_set_enable(priv->vdda18_reg, false);
 	regulator_set_enable(priv->vdd_reg, false);
 
 	return ret;
